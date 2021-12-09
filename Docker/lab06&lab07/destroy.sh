66a4403c820ff06bc6eab02f7dad6c6ced1e27ee
krasyg <84388046+krasyg@users.noreply.github.com>
2021-12-09 15:29:16 +0200

    Update destroy.sh

--- Docker/lab06&lab07/destroy.sh
+++ Docker/lab06&lab07/destroy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-for i in $(seq 1 3) do
+for i in $(seq 1 3) 
+do
     echo $(docker stop togo-manager$i)
     echo $(docker rm todo-manager$i)
 done
@@ -8,7 +9,8 @@ done
 docker volume rm todo-vol
 docker volume create --name todo-vol
 
-for i in $(seq 1 3) do 
+for i in $(seq 1 3) 
+do 
     echo $(docker create -p 300$i:3000 --name todo-manager$i \
     --mount source=todo-vol,target=/etc/todos todo-manager-img)
 done
